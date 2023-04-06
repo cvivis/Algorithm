ccfe7c3480850126b159c296ea8df261d6964291
Hwang Si-Eun <hsy0754@naver.com>
2023-04-06 16:58:00 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 664 ms, Memory: 90364 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ 
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static int T;
+    static int N, R;
+    static long result;
+    static long D[];
+    static long p = 1_000_000_007l;
+    static int size =  4_000_000;
+ 
+    private static void fac() {
+        D = new long[size+1];
+        D[0] = 1;
+        for (int i = 1; i <= size; i++) {
+            D[i] = D[i - 1] * i % p;
+        }
+    }
+ 
+    private static long pow(long base, long exp) {
+        if (exp == 0)
+            return 1;
+        else if (exp == 1)
+            return base;
+        long res = pow(base,exp/2) % p;
+        res = res * res % p;
+        if(exp % 2 == 1)res = res * base % p;
+        return res;
+         
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        int t;
+        fac();
+        for (t = 1; t <= T; t++) {
+            long result = 0;
+            tokens = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(tokens.nextToken());
+            R = Integer.parseInt(tokens.nextToken());
+            long num = D[N] ;
+            long num1= D[R] * D[N-R] % p;
+            result = num * pow(num1,p-2) %p;
+            output.append(result + "\n");
+             
+        }
+        System.out.println(output);
+    }
+}
