7a6fe2f29a79ebc20550277a8410916152a7a6dd
Hwang Si-Eun <hsy0754@naver.com>
2024-01-23 20:49:52 +0900

    [Bronze II] Title: 트럭 주차, Time: 76 ms, Memory: 11340 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -9,39 +9,43 @@ public class Main {
     static StringTokenizer tokens;
     static StringBuilder output = new StringBuilder();
     static int a,b,c;
-    static int[] arr ;
+    static int[] time;
+
     public static void main(String[] args) throws IOException {
         tokens = new StringTokenizer(br.readLine());
-        arr = new int[101];
         a = Integer.parseInt(tokens.nextToken());
         b = Integer.parseInt(tokens.nextToken());
         c = Integer.parseInt(tokens.nextToken());
+        time = new int[101];
         for(int i = 0; i < 3;i++){
             tokens = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(tokens.nextToken());
             int end = Integer.parseInt(tokens.nextToken());
-            parking(start,end);
+            parking(start, end);
         }
-        System.out.println(check());
+//        System.out.println(Arrays.toString(time));
+        System.out.println(checkMoney());
     }
-    private static void parking(int start, int end){
-        for(int i = start;i<end;i++){
-            arr[i] = arr[i]+1;
+
+    private static void parking(int start , int end){
+        for(int i = start; i < end;i++){
+            time[i]++;
         }
     }
-    private static int check(){
-        int total = 0;
-        for(int i = 1;i<arr.length;i++){
-            if(arr[i] == 1){
-                total += a;
+
+    private static int checkMoney(){
+        int cost = 0;
+        for(int i = 1; i < time.length;i++){
+            if(time[i] == 1){
+                cost += a;
             }
-            else if(arr[i] == 2){
-                total += b * 2;
+            else if(time[i] == 2){
+                cost += (b * 2);
             }
-            else if(arr[i] == 3){
-                total +=    c * 3;
+            else if(time[i] == 3){
+                cost += (c * 3);
             }
         }
-        return total;
+        return cost;
     }
 }
