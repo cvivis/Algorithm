9b20bb09b1791507b00a5dc13960abd7c2c99ebf
Hwang Si-Eun <hsy0754@naver.com>
2024-02-16 10:17:47 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 72 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver IV/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver IV/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,16 +20,14 @@ public class Main {
         System.out.println(eratosthenes());
     }
     private static int eratosthenes(){
-        for(int i = 2; i  <= N;i++ ){
-            int mul = 1;
+        for(int i = 2; i  <= N;i++ ){ // 소수자체도 세야하기 때문에 i*i 범위는 합성수만 체크할 때 사용가능
+            int mul = 1; //소수자체도 세야함
             int num = i;
-            while(num * mul <= N){
+            while(num * mul <= N){ // 배수 체크
                 if(!table[num * mul]){
-//                    System.out.println(num * mul);
                     table[num * mul] = true;
                     count++;
                     if(count == K){
-//                        System.out.println(num * mul +" asdf");
                         return num * mul;
                     }
                 }
