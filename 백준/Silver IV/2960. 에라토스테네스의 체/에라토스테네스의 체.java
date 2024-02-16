e29a6c27e51517cec8789e45ad47b84a05eb9fa3
Hwang Si-Eun <hsy0754@naver.com>
2024-02-16 10:16:11 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver IV/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver IV/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -3,45 +3,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer tokens;
-	static StringBuilder output = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static StringBuilder output = new StringBuilder();
+    static int N,K;
+    static boolean[] table;
+    static int count = 0;
 
-	
-	
-	public static void main(String[] args) throws IOException {
-		tokens = new StringTokenizer(input.readLine());
-		int N = Integer.parseInt(tokens.nextToken());
-		int K = Integer.parseInt(tokens.nextToken());
-		int[] arr = new int[N+1];
-		arr[2]= 1;
-		int count = 1;  //결과 확인용
-		int result = 2;
-		boolean finish = false;
-		for(int i = 2; i <= N; i++) {
-			int checkingNum = i;
-			int forMul = 1;
-			while(true) {
-				if(checkingNum >N)break;
-				if(arr[checkingNum] != 1) {
-					arr[checkingNum] = 1;
-					count++;
-					result = checkingNum;
-				}
-				if(count == K) {
-					finish = true;
-					break;
-				}
-				forMul++;
-				checkingNum = i * forMul;
-			}
-			if(finish)break;
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        tokens = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tokens.nextToken());
+        K = Integer.parseInt(tokens.nextToken());
+        table = new boolean[N+1];
+        System.out.println(eratosthenes());
+    }
+    private static int eratosthenes(){
+        for(int i = 2; i  <= N;i++ ){
+            int mul = 1;
+            int num = i;
+            while(num * mul <= N){
+                if(!table[num * mul]){
+//                    System.out.println(num * mul);
+                    table[num * mul] = true;
+                    count++;
+                    if(count == K){
+//                        System.out.println(num * mul +" asdf");
+                        return num * mul;
+                    }
+                }
+                mul++;
+            }
+        }
+        return 0;
+    }
 }
