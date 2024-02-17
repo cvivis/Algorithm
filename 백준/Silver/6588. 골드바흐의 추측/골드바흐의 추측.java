8f43f3c9b4252fd63a78cac37b505a46b9fd89da
Hwang Si-Eun <hsy0754@naver.com>
2024-02-17 13:20:07 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 468 ms, Memory: 63032 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,7 +10,7 @@ import java.util.StringTokenizer;
 
 // 소수테이블을 만들고
 // 1000000 만 이하의 짝수들중 가장 작은 소수를 빼고 나머지도 소수인것을 검증하는 배열을 만들자
-// 한번에 모든 짝수의 홀수의 소수 합을 구하는 minus arr를 만들려고 했지만 minus값이 천만까지
+// 한번에 모든 짝수의 홀수의 소수 합을 구하는 minus arr를 만들어서 진행 -> 8% 시간초과
 // 주어진 테스트케이스는 10만개로 한번에 천만개 구하기 보다 그때그때 구해서 10만개 구하는것이 더 효율적
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -50,9 +49,17 @@ public class Main {
                 mul++;
             }
         }
-        for(int i = 3; i <= 1000000;i++){
-            if(!prime[i])primeList.add(i);
-        }
     }
+    // 한번에 만들기
+   //    private static void findMinusPrime(){
+//        for(int i = 6;i<=100000;i++){
+//            for(int j = 3; j <= i;j++){
+//                if(!prime[j]&&!prime[i - j]){
+//                    minus[i] = j;
+//                    break;
+//                }
+//            }
+//        }
+//    }
 
 }
