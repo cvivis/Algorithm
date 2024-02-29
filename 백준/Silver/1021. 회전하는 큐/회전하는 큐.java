936c99f8a6bfd5b683ba6739548616fdca43d4cf
Hwang Si-Eun <hsy0754@naver.com>
2024-02-29 09:11:02 +0900

    [Silver III] Title: 회전하는 큐, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,10 +1,10 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+// 1차 -> 인덱스를 왼쪽부분 오른쪽 부분 따로 배열에 저장후 계산하여 풀기
+// 2차 중간 인덱스를 기준으로 그냥 큐 돌리기
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
@@ -37,18 +37,18 @@ public class Main {
             int middle = list.size()/2 ; // 까지는 left
             int count = 0;
 //            System.out.println(list + " "  +result);
-            if(list.indexOf(arr[i]) <= middle){
-                while(list.peekFirst() != arr[i]){
+            if(list.indexOf(arr[i]) <= middle){ // 빼야 하는 값이 중간보다 작으면 
+                while(list.peekFirst() != arr[i]){ // 계속 해당하는 부분나올때까지 밀기
                     list.offerLast(list.pollFirst());
                     count++;
                 }
             }
-            else{
+            else{ // 반대쪽 밀기
                 while(list.peekFirst() != arr[i]){
                     list.offerFirst(list.pollLast());
                     count++;
                     }
-                }
+                } // 밀고 맨 왼쪽 빼기 
                 list.pollFirst();
                 result += count;
             }
