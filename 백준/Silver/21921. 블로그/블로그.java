9c390f81eee34869c3c64840265318e83542a3e1
Hwang Si-Eun <hsy0754@naver.com>
2024-03-01 00:00:11 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 42624 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -22,27 +22,26 @@ public class Main {
         X = Integer.parseInt(tokens.nextToken());
         tokens = new StringTokenizer(br.readLine());
         arr = new int[N];
-        int max = 0;
+
         int sum = 0;
-        for(int i = 0; i < N;i++){
+        for(int i = 0; i < N;i++){ // 입력받기
             arr[i] = Integer.parseInt(tokens.nextToken());
         }
-        for(int i = 0; i < X;i++){
+        for(int i = 0; i < X;i++){ // 시작 윈도우 만들기
             window.add(arr[i]);
             sum += arr[i];
         }
-        max = sum;
-        count = 1;
-        for(int i = X; i < N;i++){
-            int num = window.poll();
-            sum -= num;
-            window.add(arr[i]);
-            sum += arr[i];
-            if(max < sum){
+        int max = sum;
+        count = 1; // 최댓값 갯수
+        for(int i = X; i < N;i++){ // 윈도우 다음값부터 확인
+            sum -= window.poll(); // 첫번째 윈도우 빼고
+            window.add(arr[i]); // 뒤에 넣기
+            sum += arr[i]; 
+            if(max < sum){ //최대값이 바뀌면 갱신해주기
                 max = sum;
-                count = 1;
+                count = 1; 
             }
-            else if(max == sum){
+            else if(max == sum){ 
                 count++;
             }
         }
