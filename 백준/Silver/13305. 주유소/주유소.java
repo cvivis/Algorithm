c8f097a13cd800d53c458a873ff693e7d6b31d45
Hwang Si-Eun <hsy0754@naver.com>
2024-03-05 11:34:17 +0900

    [Silver III] Title: 주유소, Time: 416 ms, Memory: 53004 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,10 +1,15 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 // 나보다 싼게 나올때까지 구매하기
+// 1차 -> 맨 마지막이 1이 아닐 수도 있다. (17점)
+// 2차 -> 리터가 1000000000이 되어 수가 굉장히 크다(58점)
+// 3차 -> cost자체와 중간에 연산되는과정도 BigInteger, Long타입을 넘긴다.(58점)
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
@@ -26,17 +31,15 @@ public class Main {
         for(int i = 0 ; i < fuel.length;i++){
             fuel[i] = Integer.parseInt(tokens.nextToken());
         }
-        int cost = 0;
+        BigInteger cost = new BigInteger(String.valueOf(0));
         for(int i = 0; i < fuel.length-1;i++){
-//            System.out.println(i+" i ");
             int disSum = dis[i];
             int next = i+1;
             while(fuel[i] < fuel[next] && next < fuel.length-1){
                 disSum+=dis[next];
                 next++;
             }
-//            System.out.println(cost);
-            cost += (fuel[i] * disSum);
+            cost = cost.add(BigInteger.valueOf((long)fuel[i] * disSum));
             i = next-1;
         }
         System.out.println(cost);
