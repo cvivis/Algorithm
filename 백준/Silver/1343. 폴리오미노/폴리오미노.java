9fa4475e74bfc4c61c46eaf5ae662deb2d746df9
Hwang Si-Eun <hsy0754@naver.com>
2024-03-05 10:31:46 +0900

    [Silver V] Title: 폴리오미노, Time: 80 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,14 +12,14 @@ public class Main {
     static char[] input;
 
     public static void main(String[] args) throws IOException {
-        input = (br.readLine()+".").toCharArray();
+        input = (br.readLine()+".").toCharArray(); // 마지막 처리를 위해 .붙이기
         int count = 0;
         boolean check = false;
         for(int i = 0; i < input.length;i++){
             if(input[i] == 'X'){
                 count++;
             }
-            else{
+            else{// .이 나올때 문자 대체하기
                 while(count > 1){
                     if(count - 4 >= 0){
                         output.append("AAAA");
@@ -31,7 +30,7 @@ public class Main {
                         count-=2;
                     }
                 }
-                if(count == 1){
+                if(count == 1){ // 1개가 남으면 될 일이 없음
                     check = true;
                     break;
                 }
